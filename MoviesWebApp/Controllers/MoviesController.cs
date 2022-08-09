3c265275c8bc3c136d672f901b5748cb21a456f1
_Saleem_ <mohamedselim701@gmail.com>
2022-08-09 09:56:25 +0200

    Add moviesController Index

--- MoviesWebApp/Controllers/MoviesController.cs
+++ MoviesWebApp/Controllers/MoviesController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApp.Models;
 
 namespace MoviesWebApp.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
         }
     }
 }
